5ebbf2d658e448108537117cad28fceebb648a47
yamaguchi <16e4q8yymg@gmail.com>
2022-09-28 07:28:01 +0900

    make it better

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,11 @@ const knex = require('../db/knex');
 const {Client} = require('pg');
 
 const client = new Client({
-  connectionString:'postgres://cygrlquuaibygd:e5690f3718f1322fa53d68fcbd76c8bc4acb36dfc83d0d84d3aee5070376c143@ec2-3-229-165-146.compute-1.amazonaws.com:5432/d58qbuc2m0e2ig',
+  host:'ec2-3-229-165-146.compute-1.amazonaws.com',
+  user:'cygrlquuaibygd',
+  password:'e5690f3718f1322fa53d68fcbd76c8bc4acb36dfc83d0d84d3aee5070376c14',
+  database:'d58qbuc2m0e2ig',
+  port: 5432,
   ssl: {
     rejectUnauthorized: false
   },
